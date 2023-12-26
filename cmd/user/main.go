8e06cfa3ab8b8414cf5528c803a13ae757ff9230
__都非拉得 <peng_du2007@qq.com>
2023-12-26 08:15:59 +0800

    update

--- cmd/user/main.go
+++ cmd/user/main.go
@@ -9,10 +9,19 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type userService struct {
 	pb.UnimplementedUserServer
+	db *gorm.DB
+}
+
+func NewUserService(db *gorm.DB) *userService {
+	return &userService{
+		db: db,
+	}
 }
 
 // Create 创建用户
@@ -25,13 +34,26 @@ func (s *userService) Create(ctx context.Context,
 	return &pb.CreateUserReply{Id: 11111}, nil
 }
 
+func NewDB() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:123456@tcp(localhost:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("cannnot connect mysql: %+v", err)
+	}
+
+	return db
+}
+
 func main() {
 	listen, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
 		log.Fatalf("err: %v", err)
 	}
+
 	s := grpc.NewServer()
-	pb.RegisterUserServer(s, &userService{})
+	db := NewDB()
+	service := NewUserService(db)
+	pb.RegisterUserServer(s, service)
+
 	if err := s.Serve(listen); err != nil {
 		log.Fatalf("err: %v\n", err)
 	}
--- go.mod
+++ go.mod
@@ -5,10 +5,15 @@ go 1.19
 require (
 	google.golang.org/grpc v1.60.1
 	google.golang.org/protobuf v1.32.0
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.5
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/net v0.16.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
